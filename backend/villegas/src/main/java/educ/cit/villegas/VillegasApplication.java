020e80ba4d1d280850db53acd455dcfc61948db8
LAB23_USER ON WS43 <L23Y19W43@LabHCI.net>
2026-09-10 18:59:45 +0800

    initial commit

---
+++ backend/villegas/src/main/java/educ/cit/villegas/VillegasApplication.java
@@ -0,0 +1,13 @@
+package educ.cit.villegas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillegasApplication.class, args);
+	}
+
+}
---
+++ backend/villegas/src/test/java/educ/cit/villegas/VillegasApplicationTests.java
@@ -0,0 +1,13 @@
+package educ.cit.villegas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillegasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
